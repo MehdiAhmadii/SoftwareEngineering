9c5dba7b52d18599f3bf06be4dfba5f38d7401a1
Mehdi Ahmadi <Ahmadi1977@gmail.com>
2023-09-03 23:41:44 +0330

    First Try

---
+++ practice1.sh
@@ -0,0 +1,11 @@
+#Extract 16:17 from logfile.csv
+cat logfile.csv  |grep 16:17
+
+#Extract all log type except  INFO
+cat logfile.csv |awk '{if ($1 = /16:17/ && $2 !="INFO") print }' FS=,
+
+#Th repetation number each class
+cat logfile.csv |awk '{if ($1 = /16:17/ && $2 ="WARN") print}'  FS=, |awk '{arr[$3]++} END {for (b in arr) {print b, arr[b]}}'
+
+#print independent class name
+cat logfile.csv |awk '{print $3}' FS=, |sort |uniq
